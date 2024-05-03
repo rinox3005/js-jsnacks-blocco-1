21735cb3b5bbbb98d6166ae31b6f77f79df8648d
rinox3005 <peppe.rinox@gmail.com>
2024-05-03 13:09:21 +0200

    [UPD] different solution

--- jsnack3/js/jsnack3.js
+++ jsnack3/js/jsnack3.js
@@ -2,12 +2,17 @@
 
 
 let sum = 0;
-const userInput = [];
+// const userInput = [];
 
-for (let i = 1; i < 11; i++) {
-    userInput[i] = Number(prompt('Enter number ' + [i]));
-    console.log('Number ' + [i] + ': ', userInput[i]);
-    sum = sum + userInput[i];
+// for (let i = 1; i < 11; i++) {
+//     userInput[i] = Number(prompt('Enter number ' + [i]));
+//     console.log('Number ' + [i] + ': ', userInput[i]);
+//     sum = sum + userInput[i];
+// }
+
+for (let i = 0; i < 10; i++) {
+
+    sum += parseInt(prompt(`Enter number ${i + 1}`));
 }
 
 console.log('The total is: ', sum);

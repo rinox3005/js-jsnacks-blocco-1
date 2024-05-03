4a3ee3e92dd97b1787f7df8d9a1fe7542a0f6a0e
rinox3005 <peppe.rinox@gmail.com>
2024-05-03 11:56:19 +0200

    [UPD] better message in console

--- jsnack2/js/jsnack2.js
+++ jsnack2/js/jsnack2.js
@@ -11,5 +11,7 @@ if (firstWord.length > secondWord.length) {
     console.log('The shortest word is:', firstWord);
     console.log('The longest word is: ', secondWord);
 } else {
+    console.log('The first word is:', firstWord);
+    console.log('The second word is:', secondWord);
     console.log('The words have the same lenght');
 }

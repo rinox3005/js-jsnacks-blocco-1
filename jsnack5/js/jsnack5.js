8d0e9476b777a3d51ea70d2c745026478d463f3d
rinox3005 <peppe.rinox@gmail.com>
2024-05-03 15:26:08 +0200

    [FIX] variable fix

--- jsnack5/js/jsnack5.js
+++ jsnack5/js/jsnack5.js
@@ -13,19 +13,19 @@ let number = 0;
 
 // -- con un ciclo for chiedo per 6 volte all'utente di inserire un numero
 
-for (let i = 1; i <= arrayLenght; i++) {
+for (let i = 0; i < arrayLenght; i++) {
 
-    let number = Number(prompt(`Enter number ${i}:`))
+    let number = Number(prompt(`Enter number ${i + 1}:`))
 
-    console.log(`Number ${i}: `, number);
+    console.log(`Number ${i + 1}: `, number);
 
     // -- controllo se l'utente ha inserito un valore numerico o se il valore inserito dall'utente non é un numero
 
     if (isNaN(number)) {
-        alert('Enter a valid number!')
+        alert('Enter a valid number!');
         i--;
-        // -- se il valore inserito dall'utente é dispari lo pusho all'interno dell'array finalArray
 
+        // -- se il valore inserito dall'utente é dispari lo pusho all'interno dell'array finalArray
     } else if (number % 2 !== 0) {
         finalArray.push(number);
     }

18467ec883a341b8ba496884691199ad2f0a67d6
rinox3005 <peppe.rinox@gmail.com>
2024-05-03 13:28:00 +0200

    [FIX] better solution with input control, updated readme

--- jsnack3/README.md
+++ jsnack3/README.md
@@ -1,5 +1,9 @@
 setto la variabile della somma a zero
 
+setto la variabile userInput a zero
+
 con un ciclo chiedo all'utente di inserire 10 numeri e di calcolare la somma totale dei numeri sommando al valore originale della somma i nuovi valori
+
+controllo se l'utente non inserisce un numero e decremento il counter se l'utente non ha inserito un numero 
 
 stampo in console il totale
--- jsnack3/js/jsnack3.js
+++ jsnack3/js/jsnack3.js
@@ -1,19 +1,24 @@
 'use strict';
 
-
+//inizializzo la variabile somma assegnando valore 0
 let sum = 0;
-// const userInput = [];
+//inizializzo la variabile userInput assegnando il valore 0
+let userInput = 0;
 
-// for (let i = 1; i < 11; i++) {
-//     userInput[i] = Number(prompt('Enter number ' + [i]));
-//     console.log('Number ' + [i] + ': ', userInput[i]);
-//     sum = sum + userInput[i];
-// }
-
+//utilizzo un ciclo per chiedere all'utente 10 volte di inserire un numero
 for (let i = 0; i < 10; i++) {
+    userInput = parseInt(prompt(`Enter number ${i + 1}:`));
 
-    sum += parseInt(prompt(`Enter number ${i + 1}`));
+    console.log('Number ' + [i + 1] + ': ', userInput);
+    //se l'utente inserisce un valore non numerico il counter diminuisce e chiedo di nuovo di inserire lo stesso numero
+    if (isNaN(userInput)) {
+        i--;
+        console.log('The value you entered is not a number, please enter a number!');
+        //sommo i valori inseriti dall'utente
+    } else {
+        sum += userInput;
+    }
 }
-
+//stampo in console il totale dei numeri
 console.log('The total is: ', sum);
 
